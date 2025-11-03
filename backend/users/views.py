ae0da30f3ee1d63ede19e63f0614342ee25da8be
attohx <nnaattoh@st.ug.edu.gh>
2025-11-03 01:50:54 +0000

    added username field to user model and created view that gets user data from jwt and exposes to api to be used

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def current_user(request):
+    user = request.user
+    user_data = {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'role': user.role,
+        'clinic': user.clinic.name if user.clinic else None,
+    }
+    return Response(user_data)
